28f1c3893e339f3fda0381093daf0bd0208b00ba
Vasyl Sokolyk <vsokolyk@gmail.com>
2023-08-21 18:06:45 +0300

    refactor(tests): upgrade test code style

--- tests/RobotsTest.php
+++ tests/RobotsTest.php
@@ -1,73 +1,74 @@
 <?php
-namespace Steein\Robots;
+
+namespace JazzMan\Robots;
 
 use PHPUnit\Framework\TestCase;
 
-class RobotsTest extends TestCase
-{
-    public function testSitemap()
-    {
-        $robots  = new Robots();
-        $sitemap = "sitemap.xml";
-        $this->assertNotContains($sitemap, $robots->render());
-        $robots->sitemap($sitemap);
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
+final class RobotsTest extends TestCase {
 
-        $this->assertContains("Sitemap: $sitemap", $robots->render());
-    }
-
-    public function testHost()
-    {
+    public function testSitemap(): void {
         $robots = new Robots();
-        $host   = "www.steein.ru";
-        $this->assertNotContains("Host: $host", $robots->render());
-        $robots->host($host);
-        $this->assertContains("Host: $host", $robots->render());
+        $sitemap = 'sitemap.xml';
+        self::assertStringNotContainsString( $sitemap, $robots->render() );
+        $robots->sitemap( $sitemap );
+
+        self::assertStringContainsString( "Sitemap: {$sitemap}", $robots->render() );
     }
 
-    public function testDisallow()
-    {
+    public function testHost(): void {
         $robots = new Robots();
-        $path = "dir";
-
-        $this->assertNotContains($path,$robots->render());
-        $robots->disallow($path);
-        $this->assertContains($path, $robots->render());
+        $host = 'www.steein.ru';
+        self::assertStringNotContainsString( "Host: {$host}", $robots->render() );
+        $robots->host( $host );
+        self::assertStringContainsString( "Host: {$host}", $robots->render() );
     }
 
-    public function testComment()
-    {
-        $robots    = new Robots();
-        $comment_1 = "Steein comment";
-        $comment_2 = "Test comment";
-        $comment_3 = "Final test comment";
+    public function testDisallow(): void {
+        $robots = new Robots();
+        $path = 'dir';
 
-        $this->assertNotContains("# $comment_1", $robots->render());
-        $this->assertNotContains("# $comment_2", $robots->render());
-        $this->assertNotContains("# $comment_3", $robots->render());
-
-        $robots->comment($comment_1);
-        $this->assertContains("# $comment_1", $robots->render());
-
-        $robots->comment($comment_2);
-        $this->assertContains("# $comment_1", $robots->render());
-        $this->assertContains("# $comment_2", $robots->render());
-
-        $robots->comment($comment_3);
-        $this->assertContains("# $comment_1", $robots->render());
-        $this->assertContains("# $comment_2", $robots->render());
-        $this->assertContains("# $comment_3", $robots->render());
+        self::assertStringNotContainsString( $path, $robots->render() );
+        $robots->disallow( $path );
+        self::assertStringContainsString( $path, $robots->render() );
     }
 
-    public function testSpacer()
-    {
+    public function testComment(): void {
+        $robots = new Robots();
+        $comment_1 = 'Steein comment';
+        $comment_2 = 'Test comment';
+        $comment_3 = 'Final test comment';
+
+        self::assertStringNotContainsString( "# {$comment_1}", $robots->render() );
+        self::assertStringNotContainsString( "# {$comment_2}", $robots->render() );
+        self::assertStringNotContainsString( "# {$comment_3}", $robots->render() );
+
+        $robots->comment( $comment_1 );
+        self::assertStringContainsString( "# {$comment_1}", $robots->render() );
+
+        $robots->comment( $comment_2 );
+        self::assertStringContainsString( "# {$comment_1}", $robots->render() );
+        self::assertStringContainsString( "# {$comment_2}", $robots->render() );
+
+        $robots->comment( $comment_3 );
+        self::assertStringContainsString( "# {$comment_1}", $robots->render() );
+        self::assertStringContainsString( "# {$comment_2}", $robots->render() );
+        self::assertStringContainsString( "# {$comment_3}", $robots->render() );
+    }
+
+    public function testSpacer(): void {
         $robots = new Robots();
 
-        $lines  = count(preg_split('/'. PHP_EOL .'/', $robots->render()));
-        $this->assertEquals(1, $lines); // Starts off with at least one line.
+        $lines = \count( preg_split( '/'.PHP_EOL.'/', $robots->render() ) );
+        self::assertSame( 1, $lines ); // Starts off with at least one line.
 
         $robots->spacer();
-        $lines = count(preg_split('/'. PHP_EOL .'/', $robots->render()));
+        $lines = \count( preg_split( '/'.PHP_EOL.'/', $robots->render() ) );
 
-        $this->assertEquals(2, $lines);
+        self::assertSame( 2, $lines );
     }
 }
